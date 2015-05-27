9cccbe08fba153226ab63eda5ca7537b5a1f52af
fcerdas <fcerdas@soin.co.cr>
2015-05-26 22:39:15 -0600

    Vemos como tambien podemos recibir parametros definidos en la ruta REST, Para este ejemplo, creamos la ruta de tipo GET llamada messages que recibe un parametro en la url (:message) y lo vamos almacenando en una arreglo llamado messages. Luego creamos otra ruta de tipo GET tambien llamada messages, nada mas que esta no recibe parametros, aqui responderemos todos los mensajes que hemos ido almacenando en el arregle messages.

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var server = express();
 
+var messages = [];
+
 server.get("/", function (req, res) {
 	res.send("Hello World!");
+});
+
+server.get("/messages", function (req, res) {
+	res.send(messages);
+});
+
+server.get("/messages/:message", function (req, res) {
+	messages.push(req.params.message);
+	res.send("tu mensaje es: " + req.params.message);
 });
 
 server.listen(3000);
