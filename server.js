5d01526c2a8a530ea1bdbb9b404725aea10d51b9
fcerdas <fcerdas@soin.co.cr>
2015-05-27 02:31:24 -0600

    Ahora si vamos con un poco de mas complejidad en nuestro desarrollo del app En este paso hacemos refactoring del codigo, y quitamos los ejemplos con los que iniciamos para poder empezar lo que es el desarrollo de nuestra aplicacion.
    Primero vamos a instalar un libreria de templates llamada swig:
    npm install swig --save
    
    Luego configuramos nuestro servidor para que utilice el engine de vistas swig.
    
    Seguido creamos la carpeta app y dentro la carpeta views
    En la carpeta de views, crearemos dos vistas, una es la vista home.html y la otra es la vista base llamada layout.html
    En el layout definimos los bloques que cambiaremos dinamicamente.

--- server.js
+++ server.js
@@ -1,31 +1,16 @@
 var express = require('express'),
-	_       = require('underscore');
+	_       = require('underscore'),
+	swig    = require('swig');
 
 var server = express();
 
-var messages = [];
+// Configuracion para renderear vistas
+server.engine('html', swig.renderFile);
+server.set('view engine', 'html');
+server.set('views', './app/views');
 
 server.get("/", function (req, res) {
-	res.send("Hello World!");
-});
-
-server.get("/messages", function (req, res) {
-	if (req.query.find && messages.length) {
-		var result = _.filter(messages, function(message){ return message == req.query.find; });
-		if (!result.length) {
-			res.send("No se encontro ningun mensaje " + req.query.find + " guardado.");
-		} else {
-			res.send(result);
-		}
-	} else {
-		var responseClient = messages.length ? messages : "Aun no has ingresado ningun mensaje";
-		res.send(responseClient);
-	}
-});
-
-server.get("/messages/:message", function (req, res) {
-	messages.push(req.params.message);
-	res.send("tu mensaje es: " + req.params.message);
+	res.render("home");
 });
 
 server.listen(3000);
