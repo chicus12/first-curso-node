03af912c955e7f7612dbf0c5924aee3ea16775dc
fcerdas <fcerdas@soin.co.cr>
2015-05-27 03:43:36 -0600

    En este paso, creamos nuestra funcion de post. Ademas agregamos dos nuevas librerias, express-session npm install express-session --save esta libreria nos permite manejar sessiones con express
    Y la libreria body-parser, que nos permite recibir los parametro del post
    npm install body-parser --save
    
    luego configuramos nuestra ruta post para que guarde en session el usuario que recibe en el request
    y redirija al usuario a la ruta app

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express'),
+	session = require('express-session'),
+	bodyParser = require('body-parser'),
 	_       = require('underscore'),
 	swig    = require('swig');
 
@@ -9,8 +11,24 @@ server.engine('html', swig.renderFile);
 server.set('view engine', 'html');
 server.set('views', './app/views');
 
+//Agregamos post, cookies y sessiones
+server.use(session({ resave: true,
+                  saveUninitialized: true,
+                  secret: 'bobcatZ7545' }));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+
 server.get("/", function (req, res) {
 	res.render("home");
+});
+
+server.get("/app", function (req, res) {
+	res.render("app", {user: req.session.user});
+});
+
+server.post("/log-in", function (req, res) {
+	req.session.user = req.body.username;
+	res.redirect('/app');
 });
 
 server.listen(3000);
