956a125868a8f2f939df85b5507fa59ee0e91352
fcerdas <fcerdas@soin.co.cr>
2015-05-27 04:18:24 -0600

    En este paso agregamos el log out Ademas realizamos algunas mejoras, como por ejemplo la lista de usuarios logeados

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
 	session = require('express-session'),
 	bodyParser = require('body-parser'),
+	cookieParser = require('cookie-parser'),
 	_       = require('underscore'),
 	swig    = require('swig');
 
@@ -15,19 +16,47 @@ server.set('views', './app/views');
 server.use(session({ resave: true,
                   saveUninitialized: true,
                   secret: 'bobcatZ7545' }));
+server.use(cookieParser());
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
-server.get("/", function (req, res) {
+var users = [];
+
+var isntLoggedIn = function (req, res, next) {
+	if (!req.session.user) {
+		res.redirect('/');
+		return;
+	}
+
+	next();
+};
+
+var isLoggedIn = function (req, res, next) {
+	if (req.session.user) {
+		res.redirect('/app');
+		return;
+	}
+
+	next();
+};
+
+server.get("/", isLoggedIn, function (req, res) {
 	res.render("home");
 });
 
-server.get("/app", function (req, res) {
-	res.render("app", {user: req.session.user});
+server.get("/app", isntLoggedIn, function (req, res) {
+	res.render("app", {user: req.session.user, clients: users});
+});
+
+server.get("/log-out", function (req, res) {
+	users = _.without(users, req.session.user);
+	req.session.destroy();
+	res.redirect('/');
 });
 
 server.post("/log-in", function (req, res) {
 	req.session.user = req.body.username;
+	users.push(req.session.user);
 	res.redirect('/app');
 });
 
