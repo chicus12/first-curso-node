46ee7f5cda33a313cf2e1452dbd91a5b4c9f7983
fcerdas <fcerdas@soin.co.cr>
2015-05-27 01:05:28 -0600

    En este paso aprendemos otra libreria importante para un adecuado flujo de trabajo en node esta libreria se llama underscore, sirve tanto para el javascript del lado del cliente, como para el javascript del lado del servidor, hay un fork independiente que se llama lodash que tambien cumple el mismo objetivo. Para instalar underscore deben ingresar en la consola lo siguiente:
    npm install underscore --save
    
    Para mas informacion de esta libreria, pueden visitar su pagina oficial y revisar su documentacion
    http://underscorejs.org/
    
    Para el caso del ejemplo utilizamos la propiedad filter de esta libreria para filtrar de un arreglo algun texto que
    queramos.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var express = require('express');
+var express = require('express'),
+	_       = require('underscore');
+
 var server = express();
 
 var messages = [];
@@ -8,8 +10,17 @@ server.get("/", function (req, res) {
 });
 
 server.get("/messages", function (req, res) {
-	debugger;
-	res.send(messages);
+	if (req.query.find && messages.length) {
+		var result = _.filter(messages, function(message){ return message == req.query.find; });
+		if (!result.length) {
+			res.send("No se encontro ningun mensaje " + req.query.find + " guardado.");
+		} else {
+			res.send(result);
+		}
+	} else {
+		var responseClient = messages.length ? messages : "Aun no has ingresado ningun mensaje";
+		res.send(responseClient);
+	}
 });
 
 server.get("/messages/:message", function (req, res) {
